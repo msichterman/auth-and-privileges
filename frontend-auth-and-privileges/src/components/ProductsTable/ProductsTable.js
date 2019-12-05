04379f937e61278daf4035209fe7acdeccc6cce3
Matt Sichterman <msichterman1@gmail.com>
2019-12-04 23:37:09 -0600

    Updated the products page by adding the jumbotron

--- frontend-auth-and-privileges/src/components/ProductsTable/ProductsTable.js
+++ frontend-auth-and-privileges/src/components/ProductsTable/ProductsTable.js
@@ -5,39 +5,39 @@ import { useSelector } from "react-redux";
 import "./ProductsTable.css";
 
 export default function ProductsTable(props) {
-    const [productsLoading, setProductsLoading] = useState(true);
+  const [productsLoading, setProductsLoading] = useState(true);
 
-    // Maps Redux store state to props
-    const loading = useSelector(state => state.auth.loading);
-    const products = useSelector(state => state.auth.products);
+  // Maps Redux store state to props
+  const loading = useSelector(state => state.auth.loading);
+  const products = useSelector(state => state.auth.products);
 
-    return loading ? (
-        <div>
-            <Spinner color="dark" />
-        </div>
-    ) : (
-        <div className="mt-4 border-top border-bottom" id="table-border-width">
-            <h3 className="text-dark p-2 mb-0">Products</h3>
-            <Table striped>
-                <thead>
-                <tr>
-                    <th>Item</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                </tr>
-                </thead>
-                <tbody>
-                {products.map(product => {
-                    return (
-                        <tr>
-                            <td>{product.name}</td>
-                            <td>{product.quantity}</td>
-                            <td>${product.price}</td>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </Table>
-        </div>
-    );
+  return loading ? (
+    <div>
+      <Spinner color="dark" />
+    </div>
+  ) : (
+    <div className="mt-4 border-top border-bottom" id="table-border-width">
+      <h3 className="text-dark p-2 mb-0">Products</h3>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => {
+            return (
+              <tr>
+                <td>{product.name}</td>
+                <td>{product.quantity}</td>
+                <td>${product.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
--- frontend-auth-and-privileges/src/containers/Products/Products.js
+++ frontend-auth-and-privileges/src/containers/Products/Products.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
-import ProductsTable from "../../components/ProductsTable/ProductsTable";
+import { Jumbotron, Container } from "reactstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../../actions/authActions";
+
+import ProductsTable from "../../components/ProductsTable/ProductsTable";
 import UpdateProduct from "../../components/UpdateProduct/UpdateProduct";
+
+import { getProducts } from "../../actions/authActions";
+
 import "./Products.css";
 
 export default function Products() {
@@ -21,7 +25,16 @@ export default function Products() {
   }
 
   return (
-    <div className="mt-4 border-top border-bottom" id="table-border-width">
+    <div className="border-top border-bottom" id="table-border-width">
+      <Jumbotron fluid className="bg-dark text-light">
+        <Container fluid>
+          <h1 className="display-5">Our Products</h1>
+          <p className="lead">
+            See information about our products below including product name,
+            quantity, and price.
+          </p>
+        </Container>
+      </Jumbotron>
       <ProductsTable />
       {role === "Admin" ? (
         <UpdateProduct heading="Update Product Price & Quantity" />
